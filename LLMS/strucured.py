81ad77896abd2d307bd1498c843a0f793a5d17e0
abhijithmr04gui <abhijithmr040405@gmail.com>
2026-07-26 17:13:37 +0530

    Modified :

--- LLMS/strucured.py
+++ LLMS/strucured.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr,Field
 from typing import Optional
 
 class Student(BaseModel):
     name:str 
     age : Optional[int] = None
     email : EmailStr 
+    CGPA : float = Field(gt=0,lt=10)
 
-new_student = {"name" : "Abhijith","age" : 21,"email" : "abhi@gmail.com"}
+new_student = {"name" : "Abhijith","age" : 21,"email" : "abhi@gmail.com","cgpa":9 }
 
 student = Student(**new_student)
 
