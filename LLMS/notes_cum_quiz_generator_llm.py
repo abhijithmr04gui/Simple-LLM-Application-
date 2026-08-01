2f387ca3ff4e06933a479c9601bc75a4482a74ef
abhijithmr04gui <abhijithmr040405@gmail.com>
2026-08-01 16:21:58 +0530

    Modified the llm

--- LLMS/notes_cum_quiz_generator_llm.py
+++ LLMS/notes_cum_quiz_generator_llm.py
@@ -1,5 +1,6 @@
-from langchain_google_genai import ChatGoogleGenerativeAI \
+from langchain_google_genai import ChatGoogleGenerativeAI 
 from langchain_groq import ChatGroq
+from langchain_core.runnables import RunnableParallel
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
@@ -18,7 +19,7 @@ prompt1 = PromptTemplate(
 )
 
 prompt2 = PromptTemplate(
-    template= "Generate a set of  5 MCQ based on the Topic \n {text}",
+    template= "Generate a set of  5 MCQ based on the Topic \n {Topic}",
     input_variables= ['text']
 )
 
@@ -26,4 +27,17 @@ prompt3 = PromptTemplate(
     template= "Merge the provided notes and MCQ into a single document , \n notes-> {notes},quiz->{quiz}",
     input_variables=['notes','quiz']
 )
+
+parallel_chain = RunnableParallel({ # Parallel chain
+    'notes': prompt1 | model1 | parser , 
+    'quiz' : prompt2 | model2 | parser
+})
+
+final_chain = prompt3|model2|parser 
+
+output  =  parallel_chain | final_chain
+
+result = output.invoke({'Topic':'Langchain'})
+
+print(result)
 
