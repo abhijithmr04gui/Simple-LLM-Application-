596b37933069ebf6325874ea6e787b52f9ba3cc9
abhijithmr04gui <abhijithmr040405@gmail.com>
2026-08-02 23:45:58 +0530

    Performed text splitting and document loader

--- LLMS/report_summarizer_llm.py
+++ LLMS/report_summarizer_llm.py
@@ -1,1 +1,41 @@
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai  import ChatGoogleGenerativeAI
+from langchain_core.runnables import RunnableLambda,RunnableSequence,RunnableParallel,RunnablePassthrough,RunnableBranch
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
+parser = StrOutputParser()
+
+prompt1 = PromptTemplate(
+    template = "Generate a report on the Topic {Topic}",
+    input_variables= ['Topic']
+)
+
+prompt2 = PromptTemplate(
+    template = "Summarize the following text {Text} ",
+    input_variables=['Text']
+)
+
+def count_word(text):
+    count = len(text.split())
+    return count 
+
+initial_chain = RunnableSequence(prompt1,model,parser)
+
+branch = RunnableBranch(
+    (RunnableLambda(lambda x : count_word(x)>5000), RunnableSequence(prompt2,model,parser)),
+    RunnablePassthrough() # default 
+)
+
+final_chain = RunnableSequence(initial_chain,branch)
+
+result = final_chain.invoke({
+'Topic':'Cricket'
+})
+print(result)
+
+
+    
